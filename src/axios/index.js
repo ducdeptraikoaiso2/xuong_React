c5991e9af685bab974d74194c2d9518387401258
ducdeptraikoaiso2 <122664189+ducdeptraikoaiso2@users.noreply.github.com>
2024-06-10 17:36:56 +0700

    B3-edit

---
+++ src/axios/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const instance = axios.create({
+	// baseURL: "https://hoangnm-json.onrender.com",
+	baseURL: "http://localhost:3000",
+	timeout: 3000,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
+
+export const getProducts = async () => {
+	(async () => {
+		try {
+			const {data} = await instance.get("/products");
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
+	})();
+}
+
+export default instance;
